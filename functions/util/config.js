76d0561f39606b1e314aeec4ce3feab4c621b11d
Ethan's Mac <ethanphanvn@gmail.com>
2020-03-16 22:18:58 -0400

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDgMLahRJ2P9nZU0ntxx6oeWKZV6x17hgk",
+  authDomain: "ape-social-media.firebaseapp.com",
+  databaseURL: "https://ape-social-media.firebaseio.com",
+  projectId: "ape-social-media",
+  storageBucket: "ape-social-media.appspot.com",
+  messagingSenderId: "42978408893",
+  appId: "1:42978408893:web:4e8bc8e52ae855edb87ad4"
+};
